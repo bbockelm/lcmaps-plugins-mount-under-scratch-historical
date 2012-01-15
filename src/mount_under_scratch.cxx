a1f2f56aeff2f0a3bdbad19ad7e7dcab975891b2
Brian Bockelman <bbockelm@cse.unl.edu>
2012-01-15 16:28:44 -0600

    Turns out, lcmaps_log.h works just fine in C++.  Switched all the logging statements from syslog to lcmaps_log.

--- src/mount_under_scratch.cxx
+++ src/mount_under_scratch.cxx
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <sstream>
 
-#include <syslog.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -10,13 +9,17 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include <lcmaps/lcmaps_log.h>
+
 #include "filesystem_remap.h"
 #include "mount_under_scratch.h"
+
+static const char * logstr = "mount-under-scratch";
 
 bool isDirectory(const char * path) {
   DIR * dirp = NULL;
   if ((dirp = opendir(path)) == NULL) {
-    syslog(LOG_ERR, "Unable to stat %s: (errno=%d) %s\n", path, errno, strerror(errno));
+    lcmaps_log(0, "%s: Unable to stat %s: (errno=%d) %s\n", logstr, path, errno, strerror(errno));
     return false;
   }
   closedir(dirp);
@@ -46,7 +49,7 @@ int mkdir_and_parents_if_needed(const char *path, mode_t mode, uid_t uid) {
   // UID switching
   uid_t orig_uid = geteuid();
   if ((orig_uid != uid) && seteuid(uid)) {
-    syslog(LOG_ERR, "Unable to set effective UID to %d: (errno=%d) %s.\n", uid, errno, strerror(errno));
+    lcmaps_log(0, "%s: Unable to set effective UID to %d: (errno=%d) %s.\n", logstr, uid, errno, strerror(errno));
     return -1;
   }
 
@@ -71,7 +74,7 @@ int mkdir_and_parents_if_needed(const char *path, mode_t mode, uid_t uid) {
 
   // Switch back
   if ((orig_uid != uid) && seteuid(orig_uid)) {
-    syslog(LOG_ERR, "Unable to return to previous UID %d: (errno=%d) %s.\n", orig_uid, errno, strerror(errno));
+    lcmaps_log(0, "%s: Unable to return to previous UID %d: (errno=%d) %s.\n", logstr, orig_uid, errno, strerror(errno));
     abort(); // We are definitely the wrong user and can't fix it.  Instant abort!
   }
   return true;
@@ -83,18 +86,7 @@ std::string dirscat(const char *dir1, const char *dir2) {
   return ss.str();
 }
 
-class SyslogManager {
-
-public:
-	SyslogManager(const std::string &ident) {openlog(ident.c_str(), 0, LOG_DAEMON);}
-
-	~SyslogManager() {closelog();}
-
-};
-
 int mount_under_scratch(uid_t uid, const char * working_dir, char *paths[]) {
-
-  SyslogManager sm("mount-under-scratch");
 
   FilesystemRemap fs_remap;
   char * path;
@@ -104,15 +96,15 @@ int mount_under_scratch(uid_t uid, const char * working_dir, char *paths[]) {
     path = paths[idx];
 
     if (!isDirectory(path)) {
-      syslog(LOG_ERR, "Unable to add mapping %s -> %s because %s doesn't exist.\n", working_dir, path, path);
+      lcmaps_log(0, "%s: Unable to add mapping %s -> %s because %s doesn't exist.\n", logstr, working_dir, path, path);
       return -1;
     }
     std::string full_dir_str = dirscat(working_dir, path);
     if (!mkdir_and_parents_if_needed( full_dir_str.c_str(), S_IRWXU, uid)) {
-      syslog(LOG_ERR, "Failed to create scratch directory %s\n", full_dir_str.c_str());
+      lcmaps_log(0, "%s: Failed to create scratch directory %s\n", logstr, full_dir_str.c_str());
       return -1;
     }
-    syslog(LOG_DEBUG, "Adding mapping: %s -> %s.\n", full_dir_str.c_str(), path);
+    lcmaps_log(4, "%s: Adding mapping: %s -> %s.\n", logstr, full_dir_str.c_str(), path);
     if (fs_remap.AddMapping(full_dir_str, path)) {
       // FilesystemRemap object prints out an error message for us.
       return -1;
